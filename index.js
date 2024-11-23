4230583521a7dbd9dacd36d6e517678af31a5f49
Manas Aggrawal <manasaggrawal@Manass-MacBook-Air.local>
2024-11-22 20:20:56 -0500

    server app

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
 };
+
 if (process.env.NODE_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
