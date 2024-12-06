401d4e47712288f7c628eca5e2774653a802c3bd
Rachana Sudhakar <rachanasudhakar17@gmail.com>
2024-12-06 17:19:14 -0500

    assignment part done people part done

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -69,4 +69,13 @@ export default function CourseRoutes(app) {
     const newAssignment = await assignmentsDao.createAssignment(assignment);
     res.send(newAssignment);
   });
+
+  const findUsersForCourse = async (req, res) => {
+    const { cid } = req.params;
+    const users = await enrollmentsDao.findUsersForCourse(cid);
+    res.json(users);
+  };
+  app.get("/api/courses/:cid/users", findUsersForCourse);
+  
+ 
 }
--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -6,8 +6,12 @@ export async function findCoursesForUser(userId) {
 }
 
 export async function findUsersForCourse(courseId) {
-  const enrollments = await model.find({ course: courseId }).populate("user");
-  return enrollments.map((enrollment) => enrollment.user);
+  try {
+    const enrollments = await model.find({ course: courseId }).populate("user");
+    return enrollments.map((enrollment) => enrollment.user);
+  } catch (err) {
+    throw new Error(`${err}`);
+  }
 }
 export function enrollUserInCourse(user, course) {
   return model.create({ user, course });
