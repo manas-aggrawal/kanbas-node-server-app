dd6f3152fb64bdebc3059ce2cb5384e7ee4ec0c3
Manas Aggrawal <manasaggrawal@Manass-MacBook-Air.local>
2024-12-08 18:41:51 -0500

    quiz

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import UserRoutes from "./Kanbas/Users/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
-import QuizRoutes from "./Kanbas/Quizzes/routes.js";
+import QuizRoutes from "./kanbas/quizzes/routes.js";
 import "dotenv/config";
 import mongoose from "mongoose";
 
