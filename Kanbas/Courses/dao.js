2dbac85249d56716bf4ccc45e1c36eea78a55856
Manas Aggrawal <manasaggrawal@Manass-MacBook-Air.local>
2024-12-11 09:50:41 -0500

    project

--- Kanbas/Courses/dao.js
+++ Kanbas/Courses/dao.js
@@ -1,6 +1,19 @@
 import model from "./model.js";
 import enrollmentsModel from "../Enrollments/model.js";
 
+function generateId() {
+  const length = Math.floor(Math.random() * 3) + 5; // Random length between 5 and 7
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let id = "";
+
+  for (let i = 0; i < length; i++) {
+    id += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  return id;
+}
+
 export function findAllCourses() {
   return model.find();
 }
@@ -16,8 +29,8 @@ export async function findCoursesForEnrolledUser(userId) {
   return enrolledCourses;
 }
 export function createCourse(course) {
-  delete course._id;
-  return model.create(course);
+  // delete course._id;
+  return model.create({ ...course, _id: generateId() });
 }
 export function deleteCourse(courseId) {
   return model.deleteOne({ _id: courseId });
